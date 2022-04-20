f7cc732c9a01808efbf5c2ac8a8b44cd1215de68
Kane20109920 <kane20109920@gmail.com>
2022-04-20 22:18:40 +0800

    Create 213. House Robber II.java

---
+++ src/DP studyplan/213. House Robber II.java
@@ -0,0 +1,39 @@
+class Solution {
+  //拆成兩個array，搶第一&搶最後，分別計算候選最大即可
+    public int rob(int[] nums) {
+        if(nums == null){
+            return 0;
+        }
+        if(nums.length == 1){
+            return nums[0];
+        }
+        if(nums.length == 2){
+            return Math.max(nums[0],nums[1]);
+        }
+        int[] robStart = new int[nums.length-1];
+        int[] robEnd = new int[nums.length-1];
+        for(int i = 0 ;i<robStart.length;i++){
+            robStart[i] = nums[i];
+        }
+        for(int i = 1;i<nums.length;i++){
+            robEnd[i-1] = nums[i];
+        }
+        return Math.max(calRob(robStart),calRob(robEnd));
+        
+    }
+    private int calRob(int[] nums){
+        if(nums.length == 1){
+            return nums[0];
+        }
+        if(nums.length == 2){
+            return Math.max(nums[0],nums[1]);
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[1],nums[0]);
+        for(int i = 2;i<dp.length;i++){
+            dp[i] = Math.max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[dp.length-1];
+    }
+}
